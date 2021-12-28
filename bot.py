45990817fb138c644c679bcba666df70d9271efe
nafi31 <nafiyadargaw360@gmail.com>
2021-12-28 17:59:37 +0300

    new changes

--- bot.py
+++ bot.py
@@ -23,7 +23,8 @@ async def reply(cls,msg):
 @bot.on_message(filters.private &filters.command("help"))
 async def reply(cls,msg):
     issue = await bot.ask(msg.from_user.id,"what type of issues are you facing please elaborate in a text we wont reply but we will release the patch A.S.A.P")
-    await bot.send_message(383694032,f"username @{msg.from_user.username} \n id {msg.from_user.id} \n bug issue {msg.text}")
+    msg.forward(383694032)
+    await bot.send_message(383694032,f"username @{msg.from_user.username} \n id {msg.from_user.id} \n bug issue {issue}")
 @bot.on_message(filters.command("download"))
 async def answer(bot,msg):
     x= await bot.ask(msg.from_user.id,"**send me the link of the youtube video **")
