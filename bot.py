5ab5dee1eca280378bc15963641d10e26ce359cd
nafi31 <nafiyadargaw360@gmail.com>
2021-12-28 17:47:03 +0300

    new changes

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from pytube import YouTube
 from pytube.exceptions import RegexMatchError
 import time,os
 from moviepy.editor import *
-bot = Client("trial2s3  ",
+bot = Client("start  ",
                      bot_token="5022200001:AAEMupSxnxJ5UjViS1Vyvud87zVUQVCGgUU",
                      api_hash="ce82c02582127129de5bf0ff2580352e",
                      api_id="1589810")
@@ -15,7 +15,7 @@ bot = Client("trial2s3  ",
 @bot.on_message(filters.private & filters.command("start"))
 async def answer(bot, message):
    
-   await message.reply(f"Hi {message.from_user.first_name} welcome to this bot press /commands to see the available commands")
+   await message.reply(f"Hi {message.from_user.first_name} welcome to this bot press \n /commands to see the available commands")
    
 @bot.on_message(filters.private &filters.command("commands"))
 async def reply(cls,msg):
