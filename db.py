bd9101ab66ea2cfcaeb8fe8e81da07f80668085f
nafi31 <nafiyadargaw360@gmail.com>
2022-01-06 20:47:59 +0300

    modified db

--- db.py
+++ db.py
@@ -4,6 +4,10 @@ conn = psycopg.connect(
 
 cur = conn.cursor()
 def create_table():
+    conn = psycopg.connect(
+    "host=abul.db.elephantsql.com  dbname=ovooekmc  user=ovooekmc password=Q0JeWjZOXDqQ1JHsvl3xfjIkZcgRWNpl ")
+
+    cur = conn.cursor()
     cur.execute('''
     CREATE TABLE  userdata(
     id serial PRIMARY KEY,
@@ -12,20 +16,34 @@ def create_table():
 
 
 def getallusers():
+    conn = psycopg.connect(
+    "host=abul.db.elephantsql.com  dbname=ovooekmc  user=ovooekmc password=Q0JeWjZOXDqQ1JHsvl3xfjIkZcgRWNpl ")
+
+    cur = conn.cursor()
     cur.execute("SELECT * FROM userdata")
     conn.commit()
     return cur.fetchall()
+    conn.close()
 def getusers(user_ids):
+    conn = psycopg.connect(
+    "host=abul.db.elephantsql.com  dbname=ovooekmc  user=ovooekmc password=Q0JeWjZOXDqQ1JHsvl3xfjIkZcgRWNpl ")
+
+    cur = conn.cursor()
     cur.execute("select * from userdata where user_id = %s",(user_ids,))
     conn.commit()
     return cur.fetchall()
 
 def add_user(user_ids):
+    conn = psycopg.connect(
+    "host=abul.db.elephantsql.com  dbname=ovooekmc  user=ovooekmc password=Q0JeWjZOXDqQ1JHsvl3xfjIkZcgRWNpl ")
+
+    cur = conn.cursor()
     if not getusers(user_ids):
 
         cur.execute("INSERT INTO userdata (user_id) values(%s)" , (user_ids,))
         conn.commit()
-        print("success")
+        conn.close()
         
     else:
+        conn.close()
         print("no si")
